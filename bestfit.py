705f0b9ec6ca68851c471ab87ab7b50318e035d0
Alex Kim <agkim@lbl.gov>
2022-09-14 15:07:07 -0700

    remove degenerate solutions

--- bestfit.py
+++ bestfit.py
@@ -8,16 +8,20 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 filename = '/Users/akim/Downloads/template_links.csv'
-
 data = numpy.loadtxt(filename,delimiter = ",",skiprows=1)
-data = data[:,2:]
 
+
+# Thien I think index L1[10] 9, L2[10] 19, f[x,x] 62 and m1[10:16] -1 cen be removed
+data = data[:,2:-1]
+data = numpy.delete(data,62,axis=1)
+data = numpy.delete(data,19,axis=1)
+data = numpy.delete(data,9,axis=1)
 
 # iteration 1
 
 mn = numpy.mean(data,axis=0)
 cov = numpy.cov(data,rowvar=False)
-invcov = numpy.linalg.pinv(cov)
+invcov = numpy.linalg.inv(cov)
 insider = []
 for i in range(data.shape[0]):
 	d = data[i,:]-mn
@@ -35,7 +39,7 @@ data = data[w,:]
 
 mn = numpy.mean(data,axis=0)
 cov = numpy.cov(data,rowvar=False)
-invcov = numpy.linalg.pinv(cov)
+invcov = numpy.linalg.inv(cov)
 insider = []
 for i in range(data.shape[0]):
 	d = data[i,:]-mn
@@ -48,8 +52,8 @@ insider = numpy.array(insider)
 # Gaussfit
 x0=numpy.zeros(2*data.shape[1])
 x0[:len(x0)//2] = mn
-x0[len(x0)//2:] = cov.diagonal()+0.1
-res = minimize(lambda x: -(multivariate_normal.logpdf(data, mean= x[:len(x)//2], cov=x[len(x)//2:])).sum(),x0)
+x0[len(x0)//2:] = cov.diagonal()
+res = minimize(lambda x: -(multivariate_normal.logpdf(data, mean= x[:len(x)//2], cov=numpy.abs(x[len(x)//2:]))).sum(),x0)
 
 
 
