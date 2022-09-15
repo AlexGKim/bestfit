7181224d7800a9d75e80cfdcc33f03452af62a5c
Alex Kim <akim@femur.local>
2022-09-15 10:02:56 -0700

    seems to work

--- bestfit.py
+++ bestfit.py
@@ -1,6 +1,7 @@
 import numpy
 from scipy.optimize import minimize
 from scipy.stats import multivariate_normal
+from sklearn.cluster import MeanShift
 import matplotlib.pyplot as plt
 import matplotlib
 
@@ -12,10 +13,10 @@ data = numpy.loadtxt(filename,delimiter = ",",skiprows=1)
 
 
 # I think index L1[10] 9, L2[10] 19, f[x,x] 62 and m1[10:16] -1 cen be removed
-data = data[:,2:-1]
+data = data[:,2:]
 data = numpy.delete(data,62,axis=1)
-data = numpy.delete(data,19,axis=1)
-data = numpy.delete(data,9,axis=1)
+# data = numpy.delete(data,19,axis=1)
+# data = numpy.delete(data,9,axis=1)
 
 # Get rid of outliers
 
@@ -38,12 +39,11 @@ w = insider < cut1
 data = data[w,:]
 
 ## Pass 2: Get rid of outliers in the most important parameters
-ndim = 25
+ndim = 50
 
 mn = numpy.mean(data,axis=0)
 cov = numpy.cov(data,rowvar=False)
 
-# Looking at the eigenvalues of cov, seems like there is information in the first 50 or so
 evalu, evec = numpy.linalg.eig(cov)
 data_red = (data-mn) @ evec[:,:ndim]
 
@@ -54,22 +54,29 @@ for i in range(data.shape[0]):
 insider = numpy.array(insider)
 # plt.hist(insider)
 # plt.show()
-cut1 = 40
+cut1 = 70
 w = insider < cut1
 data = data[w,:]
 
-# Gaussfit
-## Fit the most important parameters first
+# Find the peak
+
+## transform the data
 
 mn = numpy.mean(data,axis=0)
 cov = numpy.cov(data,rowvar=False)
 
 evalu, evec = numpy.linalg.eig(cov)
-data_red = (data-mn) @ evec[:,:ndim]
+data_red = ((data-mn) @ evec)/numpy.sqrt(evalu)
 
-x0=numpy.zeros(2*data_red.shape[1])
-x0[len(x0)//2:] = evalu[ndim]
-res = minimize(lambda x: -(multivariate_normal.logpdf(data_red, mean= x[:len(x)//2], cov=numpy.abs(x[len(x)//2:]))).sum(),x0)
+## determine centers
+clustering = MeanShift().fit(data_red)
 
+## transform back
+ans = clustering.cluster_centers_[0,:]*numpy.sqrt(evalu)+mn
 
-print(res)
+# ans=numpy.insert(ans,9,numpy.sqrt(1-(ans[:9]**2).sum()))
+# ans=numpy.insert(ans,19,numpy.sqrt(1-(ans[10:19]**2).sum()))
+ans=numpy.insert(ans,62,1)
+# ans=numpy.append(ans,numpy.sqrt(160-ans[-159:].sum()))
+
+print(ans)
